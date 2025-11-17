849a4b63696eccec3c0b895edfe877fa7016a4ef
sancho1610 <sanzarzumadilov487@gmail.com>
2025-11-18 01:22:04 +0600

    dz

--- dz7/js/lesson.js
+++ dz7/js/lesson.js
@@ -188,30 +188,3 @@ searchButton.onclick = async () => {
 
 // 
 
-const container = document.querySelector(".cards-container");
-
-const API_URL = "https://6919fb0c9ccba073ee948461.mockapi.io/dz";
-
-async function loadCards() {
-    try {
-        const response = await fetch(API_URL);
-        const cards = await response.json();
-
-        container.innerHTML = "";
-
-        cards.forEach(card => {
-            container.innerHTML += `
-                <div class="cards">
-                    <h2>${card.name}</h2>
-                    <img src="${card.avatar}" alt="">
-                    <p>${card.text}</p>
-                </div>
-            `;
-        });
-
-    } catch (e) {
-        console.log("Ошибка загрузки", e);
-    }
-}
-
-loadCards();
